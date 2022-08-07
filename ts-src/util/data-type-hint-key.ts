65ebfc3260e82a9aed2e7aac5a8a34f0c183a0cd
Franz Zemen <franzzemen@gmail.com>
2022-08-07 08:07:59 -0400

    data type hint key

--- ts-src/util/data-type-hint-key.ts
+++ ts-src/util/data-type-hint-key.ts
@@ -1,7 +1,7 @@
 import {HintKey} from '@franzzemen/re-common/util/hint-key';
 
 export class DataTypeHintKey extends HintKey {
-  public static DataType = 'DataType';
+  public static DataType = 'data-type';
   public static DataTypeModule = 'data-type-module';
   public static DataTypeModuleName = 'data-type-module-name';
   public static DataTypeFunctionName = 'data-type-function-name';
