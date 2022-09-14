70a3877f665aed0be85b6e3a081d2b72e1202979
Franz Zemen <franzzemen@gmail.com>
2022-09-14 08:01:24 -0400

    hint constants

--- ts-src/util/data-type-hint-key.ts
+++ ts-src/util/data-type-hint-key.ts
@@ -6,4 +6,6 @@ export class DataTypeHintKey extends HintKey {
   public static DataTypeModuleName = 'data-type-module-name';
   public static DataTypeFunctionName = 'data-type-function-name';
   public static DataTypeConstructorName = 'data-type-constructor-name';
+  public static DataTypeModuleResolutionName = 'data-type-module-resolution';
+  public static DataTypeLoadSchemaName = 'data-type-load-schema';
 }
