3d2bd6eb15ec711db6307b057411f3072184ec4f
Franz Zemen <franzzemen@gmail.com>
2022-09-20 17:47:02 -0400

    hints constants

--- ts-src/util/data-type-hint-key.ts
+++ ts-src/util/data-type-hint-key.ts
@@ -6,8 +6,10 @@ export class DataTypeHintKey extends HintKey {
   public static DataTypeModuleName = 'data-type-module-name';
   public static DataTypeFunctionName = 'data-type-function-name';
   public static DataTypeConstructorName = 'data-type-constructor-name';
-  public static DataTypeModuleResolutionName = 'data-type-module-resolution';
-  public static DataTypeLoadSchemaName = 'data-type-load-schema';
+  public static DataTypeModuleResolution = 'data-type-module-resolution';
+  public static DataTypeLoadSchema = 'data-type-load-schema';
+  // This is a unary hint - presence means true
   public static DataTypeModuleOverride = 'data-type-module-override';
+  // This is a unary hint - presence means true
   public static DataTypeModuleOverrideDown = 'data-type-module-override-down';
 }
