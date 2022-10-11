8bbaabb64388a2622ab32ec00fe90dc792d6f6c3
Franz Zemen <franzzemen@gmail.com>
2022-10-11 19:52:10 -0400

    git

--- ts-src/cli.ts
+++ ts-src/cli.ts
@@ -1,13 +1,19 @@
 import {ExecutionContextI, LoggerAdapter} from '@franzzemen/app-utility';
-import {defaultCliFactory, execute, logParserMessages} from '@franzzemen/re-common/cli-common.js';
+import {CliFunction, defaultCliFactory, execute, logParserMessages} from '@franzzemen/re-common/cli-common.js';
 import {DataTypeFactory} from './factory/data-type-factory.js';
 import {DataTypeInferenceStackParser} from './literal-parser/data-type-inference-stack-parser.js';
 import {DataTypeScope} from './scope/data-type-scope.js';
 
 export const dataTypeExecutionKey = 're-data-type';
 
-function executeDataTypeCLI(iteration: string, ec?: ExecutionContextI) {
+export interface DataTypeCliFormat {
+  dataTypeRef?: string;
+  text: string;
+}
+
+export const executeDataTypeCLI: CliFunction<DataTypeCliFormat> = (iteration: DataTypeCliFormat, ec?: ExecutionContextI) => {
   const log = new LoggerAdapter(ec, 're-data-type', 'cli', 'executeDataTypeCLI');
+  /*
   let dataRegex = /^(([a-zA-Z]+)[\s\t\r\n\v\f\u2028\u2029]+([a-zA-Z0-9.\-:]+|["']{1}[a-zA-Z0-9.\-:\s\t\r\n\v\f\u2028\u2029]+["']{1}))|([a-zA-Z0-9.\-:]+|["']{1}[a-zA-Z0-9.\-:\s\t\r\n\v\f\u2028\u2029]+["']{1})$/;
   let result2 = dataRegex.exec(iteration);
   let dataStr: string, dataTypeRef: string;
@@ -22,39 +28,38 @@ function executeDataTypeCLI(iteration: string, ec?: ExecutionContextI) {
     log.error('Inconsistent section, cannot determine data string');
     process.exit(8);
   }
-  if (dataStr) {
-    try {
-      const scope: DataTypeScope = new DataTypeScope({}, undefined, ec);
-      if (dataTypeRef) {
-        const dataTypeFactory: DataTypeFactory = scope.get(DataTypeScope.DataTypeFactory);
-        if (!dataTypeFactory.hasRegistered(dataTypeRef, ec)) {
-          // TODO:  Account for custom types?  Or is that in Expression?
-          log.warn(`Input data type ref ${dataTypeRef} does not exist, so ignoring and inferring`);
-          dataTypeRef = undefined;
-        } else {
-          log.info(`Constraining to data type ${dataTypeRef}`);
-        }
+  */
+  let dataTypeRef = iteration.dataTypeRef;
+  let dataStr = iteration.text;
+  try {
+    const scope: DataTypeScope = new DataTypeScope({}, undefined, ec);
+    if (dataTypeRef) {
+      const dataTypeFactory: DataTypeFactory = scope.get(DataTypeScope.DataTypeFactory);
+      if (!dataTypeFactory.hasRegistered(dataTypeRef, ec)) {
+        // TODO:  Account for custom types?  Or is that in Expression?
+        log.warn(`Input data type ref ${dataTypeRef} does not exist, so ignoring and inferring`);
+        dataTypeRef = undefined;
+      } else {
+        log.info(`Constraining to data type ${dataTypeRef}`);
       }
-      const parser = scope.get(DataTypeScope.DataTypeInferenceStackParser) as DataTypeInferenceStackParser;
-      let [remaining, [value, parserRefName], parserMessages] = parser.parse(dataStr, scope, dataTypeRef, ec);
-      logParserMessages(parserMessages, ec);
-      if (value !== undefined) {
-        log.info(`Data type is "${parserRefName}" with value ${parserRefName === 'Text' ? '\"' + value + '\"' : value}`);
-      }
-      if (remaining && remaining.trim().length > 0) {
-        log.info(`Remaining: ${remaining}`);
-      }
-      if (value === undefined) {
-        log.info('No parsable data type literal')
-      }
-    } catch (err) {
-      log.error(err);
     }
-  } else {
-    log.error(new Error(`No data to parse`));
-    process.exit(9);
+    const parser = scope.get(DataTypeScope.DataTypeInferenceStackParser) as DataTypeInferenceStackParser;
+    let [remaining, [value, parserRefName], parserMessages] = parser.parse(dataStr, scope, dataTypeRef, ec);
+    logParserMessages(parserMessages, ec);
+    if (value !== undefined) {
+      log.info(`Data type is "${parserRefName}" with value ${parserRefName === 'Text' ? '\"' + value + '\"' : value}`);
+    }
+    if (remaining && remaining.trim().length > 0) {
+      log.info(`Remaining: ${remaining}`);
+    }
+    if (value === undefined) {
+      log.info('No parsable data type literal');
+    }
+  } catch (err) {
+    log.error(err);
   }
-}
+};
+
 
 defaultCliFactory.register({
   instanceRef: {
@@ -64,5 +69,5 @@ defaultCliFactory.register({
 });
 
 if (process.argv[2] === dataTypeExecutionKey) {
-  execute();
+  execute<DataTypeCliFormat>();
 }
